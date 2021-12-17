ab44355153634e93bd59436cd9f3b453c7432d2e
Ernesto2899 <eramirez28@ucol.mx>
2021-12-17 14:09:57 -0600

    Creacion of the project and creacion of the list.json

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Image } from "react-native";
+import { Icon } from "react-native-elements/dist/icons/Icon";
+export const Card = (propertiesList) => {
+
+//    const propertiesDisplay = propertiesList.map((item) => {
+//        return(
+//            <ItemContainer>
+
+  //          </ItemContainer>
+  //      )
+    //})
+
+
+    return (
+        <View>
+            
+           <Icon name = "home" size = {30}/>
+        </View>
+    );
+};
---
+++ src/screens/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text } from "react-native";
+import { Card } from '../components'
+
+export const Home = () => {
+    
+    
+    return(
+        <>
+            <Text>Home</Text>
+            <Card />
+        </>
+    );
+};
+

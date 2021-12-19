59b3b6efcd81e58f5b2738d805c9d4aa9d2fc93a
Ernesto2899 <eramirez28@ucol.mx>
2021-12-18 20:18:01 -0600

    Taka Completed

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Photo, Box, Box2, Imgback, Nombre, Icons, Direccion} from "./src/styled.js";
+import {Container, Photo, Vista, Box, Down, Box2, Imgback, Nombre, Place, Small, Icons, Direccion, Money, Rango, Iconimg} from "./src/styled.js";
 import List from "./src/list.json";
 import { View, Text, FlatList} from "react-native";
 
@@ -8,18 +8,27 @@ const Card = ({name, address, rooms, bathrooms, feets, price, ratings, img  }) =
   return (
   <Box>
     <Imgback source={{uri:img}}>
-      <Photo source={require("./src/images/star.png")} />
-      <Text>{ratings}</Text>
+      <Vista>
+        <Rango> 
+          <Photo source={require("./src/images/star.png")} /> {ratings}
+        </Rango>
+      </Vista>
     </Imgback>
     <Box2>
       <Nombre>{name}</Nombre>
-      <Direccion>{address}</Direccion>
+      <Direccion><Place source={require("./src/images/placeholder.png")} />{address}</Direccion>
       <Icons>
-        <Text>{rooms}</Text>
-        <Text>{bathrooms}</Text>
-        <Text>{feets}</Text>
+        <Iconimg source={require("./src/images/bed.png")} />
+        <Small>{rooms} </Small>
+        <Iconimg source={require("./src/images/bath.png")} />
+        <Small>{bathrooms}</Small>
+        <Iconimg source={require("./src/images/length.png")} />
+        <Small>{feets}</Small>
       </Icons>
-      <Text>{price}</Text>
+      <Down>
+      <Money>{price}</Money>
+      <Photo source={require("./src/images/heart2.png")} />
+      </Down>
     </Box2>
     
     
--- src/styled.js
+++ src/styled.js
@@ -2,41 +2,44 @@ import styled from 'styled-components/native';
 import {View, Image, ImageBackground, Text} from 'react-native';
 
 export const Container = styled.View `
-    padding: 20px;
+    padding: 15px;
 `;
 
 export const Photo = styled.Image `
-        width: 40px;
-        height: 40px;
-        alignItems: center;
+        width: 15px;
+        height: 15px;
+
 `;
 
 export const Box = styled.View`
-    flex-direction: row;
-    flex: 1;
-    padding: 15px;
-    background-color: #E5F9FE;
-    margin-bottom: 20px;
-    border-radius: 35px;
+        flex-direction: row;
+        flex: 1;
+        padding: 15px;
+        background-color: #E5F9FE;
+        margin-bottom: 20px;
+        border-radius: 30px;
 `;
 
 export const Box2 = styled.View`
-    flex-direction: column;
-    flex: 1;
+        flex-direction: column;
+        flex: 1;
 `;
 
 
 export const Imgback = styled.ImageBackground`
         flex: 1;
-        border-radius: 25px;
+        border-radius: 20px;
         overflow: hidden;
         margin-right: 10px;
+        justify-content: flex-end;
+        alignItems: center;
 `;
 
 export const Nombre = styled.Text`
         font-size: 20px;
+        fontWeight: 400;
         color: black;
-        fontWeight: bold;
+        
 `;
 
 export const Icons = styled.View`
@@ -48,4 +51,48 @@ export const Icons = styled.View`
 export const Direccion = styled.Text`
         font-size: 15px;
         color: gray;
+        fontWeight: 400;
 `;
+
+export const Money = styled.Text`
+        fontWeight: bold;
+        color: black;
+        font-size: 16px;  
+`;
+
+export const Small = styled.Text`
+        font-size: 14px;
+        color: black;
+        fontWeight: 900;
+`;
+
+export const Rango = styled.Text`
+        fontWeight: bold;
+        padding: 3px;
+        borderRadius: 12px;
+        backgroundColor: #F9E9AD; 
+`;
+
+export const Iconimg = styled.Image`
+        justify-content: space-between;
+        width: 22px;
+        height: 22px;
+`;
+
+export const Vista = styled.View`
+        margin-bottom: 10px;
+        flex-wrap: wrap;
+`;
+
+export const Place = styled.Image`
+        width: 18px;
+        height: 18px;
+`;
+
+export const Down = styled.View`
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        alignItems: center;
+`;
+
